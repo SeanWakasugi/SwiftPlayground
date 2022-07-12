6aaa7b3101a4a239d91a9e754231da33ae9b9724
SeanWakasugi <106071820+SeanWakasugi@users.noreply.github.com>
2022-07-12 16:00:08 +0900

    UserNotifications完成

--- SwiftPlayground/ViewController.swift
+++ SwiftPlayground/ViewController.swift
@@ -6,90 +6,73 @@
 //
 
 import UIKit
+import UserNotifications
 
 class ViewController: UIViewController {
     
-    let label = UILabel()
-    let plainButton = UIButton(type: .system)
-    let grayButton = UIButton(type: .system)
-    let tintedButton = UIButton(type: .system)
-    let filledButton = UIButton(type: .system)
-    let pullDownButton = UIButton(type: .system)
-    let popUpButton = UIButton(type: .system)
-
-    let stackView = UIStackView()
+    let testNotificationButton = UIButton(type: .system)
+    let setNotificationButton = UIButton(type: .system)
+    let datePicker = UIDatePicker()
     
-    @IBOutlet weak var sbPullDownButton: UIButton!
-    @IBOutlet weak var sbPopUpButton: UIButton!
+    let stackView = UIStackView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let center = UNUserNotificationCenter.current()
+        center.delegate = self
+        center.requestAuthorization(options: [.alert, .sound], completionHandler: { (granted, error) in })
+        
         view.addSubview(stackView)
         stackView.translatesAutoresizingMaskIntoConstraints = false
         stackView.axis = .vertical
         stackView.alignment = .center
-        stackView.spacing = 10
-        stackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 50).isActive = true
-        stackView.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor).isActive = true
-
-        label.text = "コードで実装したボタン"
-        stackView.addArrangedSubview(label)
+        stackView.spacing = 20
+        stackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 100).isActive = true
+        stackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 30).isActive = true
+        stackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -30).isActive = true
         
-        plainButton.configuration = UIButton.Configuration.plain()
-        grayButton.configuration = UIButton.Configuration.gray()
-        tintedButton.configuration = UIButton.Configuration.tinted()
-        filledButton.configuration = UIButton.Configuration.filled()
+        stackView.addArrangedSubview(testNotificationButton)
+        testNotificationButton.setTitle("通知のテスト", for: .normal)
+        testNotificationButton.configuration = UIButton.Configuration.plain()
+        testNotificationButton.addTarget(self, action: #selector(testNotification(sender:)), for: .touchUpInside)
         
-        addButton(uiButton: plainButton)
-        addButton(uiButton: grayButton)
-        addButton(uiButton: tintedButton)
-        addButton(uiButton: filledButton)
-
-        addPullDownButton(pullDownButton: pullDownButton)
-        addPopUpButton(popUpButton: popUpButton)
-
-        // StoryboardのPullDown/PopUpボタンにもメニュー設定
-        setPullDownMenu(pullDownButton: sbPullDownButton)
-        setPopUpMenu(popUpButton: sbPopUpButton)
+        stackView.addArrangedSubview(datePicker)
+        datePicker.preferredDatePickerStyle = .inline
         
+        stackView.addArrangedSubview(setNotificationButton)
+        setNotificationButton.configuration = UIButton.Configuration.filled()
+        setNotificationButton.setTitle("通知日時設定", for: .normal)
+        setNotificationButton.addTarget(self, action: #selector(setNotification(sender: )), for: .touchUpInside)
     }
     
-    func addButton(uiButton: UIButton) {
-        stackView.addArrangedSubview(uiButton)
-        uiButton.setTitle("Button", for: .normal)
+    @objc func testNotification(sender: UIButton) {
+        let trigger = UNTimeIntervalNotificationTrigger.init(timeInterval: 0.5, repeats: false)
+        showNotification(trigger: trigger)
     }
     
-    
-    func addPullDownButton(pullDownButton: UIButton) {
-        setPullDownMenu(pullDownButton: pullDownButton)
-        pullDownButton.showsMenuAsPrimaryAction = true
-        pullDownButton.setTitle("Button", for: .normal)
-        stackView.addArrangedSubview(pullDownButton)
+    @objc func setNotification(sender: UIButton) {
+        let dateComponents = Calendar.current.dateComponents(in: TimeZone.current, from: datePicker.date)
+        let trigger = UNCalendarNotificationTrigger.init(dateMatching: dateComponents, repeats: false)
+        showNotification(trigger: trigger)
     }
     
-    func setPullDownMenu(pullDownButton: UIButton) {
-        let items = UIMenu(children: [
-            UIAction(title: "Edit", image: UIImage(systemName: "pencil"), handler: { _ in print("Edit is tapped")}),
-            UIAction(title: "Disabled", attributes: .disabled, handler: { _ in print("Disabled is tapped")}),
-            UIAction(title: "Hidden", attributes: .hidden, handler: { _ in print("Hidden is tapped")}),
-            UIAction(title: "Delete", image: UIImage(systemName: "trash"), attributes: .destructive, handler: { _ in print("Delete is tapped")})
-        ])
-        pullDownButton.menu = items
-    }
-    
-    func addPopUpButton(popUpButton: UIButton) {
-        setPopUpMenu(popUpButton: popUpButton)
-        popUpButton.showsMenuAsPrimaryAction = true
-        popUpButton.changesSelectionAsPrimaryAction = true
-        stackView.addArrangedSubview(popUpButton)
-    }
-    
-    func setPopUpMenu(popUpButton: UIButton) {
-        let items = UIMenu(children: [
-            UIAction(title: "Online Mode", handler: { _ in print("Online mode")}),
-            UIAction(title: "Offline Mode", handler: { _ in print("Offline mode")})
-        ])
-        popUpButton.menu = items
+    func showNotification(trigger: UNNotificationTrigger) {
+        let content = UNMutableNotificationContent()
+        content.title = "Notification"
+        content.body = "This is a notification"
+        content.sound = UNNotificationSound.default
+        let trigger = trigger
+        let uuidString = UUID().uuidString
+        let request = UNNotificationRequest.init(identifier: uuidString, content: content, trigger: trigger)
+        let center = UNUserNotificationCenter.current()
+        center.add(request) { (error) in print(error as Any) }
+        print(trigger)
     }
 }
 
+extension ViewController: UNUserNotificationCenterDelegate {
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        completionHandler([.banner, .sound])
+    }
+}

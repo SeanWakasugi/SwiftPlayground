d8f094f6708ceec815f95831e765d4b0e2ee4764
SeanWakasugi <106071820+SeanWakasugi@users.noreply.github.com>
2022-08-10 14:42:20 +0900

    時刻表示まで完成

--- SwiftPlayground/CountAndTimeViewController.swift
+++ SwiftPlayground/CountAndTimeViewController.swift
@@ -13,9 +13,33 @@ class CountAndTimeViewController: UIViewController {
     @IBOutlet weak var timeLabel: UILabel!
     @IBOutlet weak var timeButton: UIButton!
     
+    var count: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        countLabel.text = String(count)
+        setTimeLabel()
     }
     
+    @IBAction func didPressCountButton(_ sender: UIButton) {
+        countUp()
+            
+    }
+    
+    @IBAction func didPressTimeButton(_ sender: UIButton) {
+        setTimeLabel()
+    }
+    
+    func countUp() {
+        count = count + 1
+        countLabel.text = String(count)
+    }
+    
+    func setTimeLabel() {
+        let formatter = DateFormatter()
+        formatter.calendar = Calendar(identifier: .gregorian)
+        formatter.dateFormat = "yyyy/MM/dd HH:mm:ss"
+        timeLabel.text = formatter.string(from: Date())
+    }
 }
